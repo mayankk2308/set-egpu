9de15564450c380f95a72aeb52b9385d87a40c6a
Mayank Kumar <mayankk2308@gmail.com>
2018-07-03 03:08:09 +0530

    Significantly improve specified app search + codebase enhancements

--- set-eGPU.sh
+++ set-eGPU.sh
@@ -6,7 +6,7 @@
 
 # ----- ENVIRONMENT
 
-# Enable case-insensitive matching & null dereferences
+# Enable null dereferences
 shopt -s nocasematch
 shopt -s nullglob
 
@@ -48,6 +48,9 @@ GPU_SELECTION_POLICY_VALUE="preferRemovable"
 # Exempt App Location(s)
 UTILITIES="/Applications/Utilities"
 
+# Found application(s) history
+APPS_LIST=()
+
 # ----- SOFTWARE UPDATES & INSTALLATION
 
 # Elevate privileges
@@ -61,7 +64,7 @@ elevate_privileges() {
 
 # Perform software update
 perform_software_update() {
-  echo "${BOLD}Downloading...${NORMAL}"
+  echo -e "${BOLD}Downloading...${NORMAL}"
   curl -L -s "${LATEST_RELEASE_DWLD}" > "${TMP_SCRIPT}"
   echo -e "Download complete.\n${BOLD}Updating...${NORMAL}"
   chmod 700 "${TMP_SCRIPT}" && chmod +x "${TMP_SCRIPT}"
@@ -85,11 +88,11 @@ prompt_software_update() {
 fetch_latest_release() {
   [[ "${BIN_CALL}" == 0 ]] && return
   LATEST_SCRIPT_INFO="$(curl -s "https://api.github.com/repos/mayankk2308/set-egpu/releases/latest")"
-  LATEST_RELEASE_VER="$(echo "${LATEST_SCRIPT_INFO}" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
-  LATEST_RELEASE_DWLD="$(echo "${LATEST_SCRIPT_INFO}" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')"
-  LATEST_MAJOR_VER="$(echo "${LATEST_RELEASE_VER}" | cut -d '.' -f1)"
-  LATEST_MINOR_VER="$(echo "${LATEST_RELEASE_VER}" | cut -d '.' -f2)"
-  LATEST_PATCH_VER="$(echo "${LATEST_RELEASE_VER}" | cut -d '.' -f3)"
+  LATEST_RELEASE_VER="$(echo -e "${LATEST_SCRIPT_INFO}" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+  LATEST_RELEASE_DWLD="$(echo -e "${LATEST_SCRIPT_INFO}" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')"
+  LATEST_MAJOR_VER="$(echo -e "${LATEST_RELEASE_VER}" | cut -d '.' -f1)"
+  LATEST_MINOR_VER="$(echo -e "${LATEST_RELEASE_VER}" | cut -d '.' -f2)"
+  LATEST_PATCH_VER="$(echo -e "${LATEST_RELEASE_VER}" | cut -d '.' -f3)"
   if [[ $LATEST_MAJOR_VER > $SCRIPT_MAJOR_VER || ($LATEST_MAJOR_VER == $SCRIPT_MAJOR_VER && $LATEST_MINOR_VER > $SCRIPT_MINOR_VER) || ($LATEST_MAJOR_VER == $SCRIPT_MAJOR_VER && $LATEST_MINOR_VER == $SCRIPT_MINOR_VER && $LATEST_PATCH_VER > $SCRIPT_PATCH_VER) && "$LATEST_RELEASE_DWLD" ]]
   then
     elevate_privileges
@@ -108,8 +111,8 @@ install_bin() {
 # Bin first-time setup
 first_time_setup() {
   [[ $BIN_CALL == 1 ]] && return
-  SCRIPT_FILE="$(pwd)/$(echo "${SCRIPT}")"
-  [[ "${SCRIPT}" == "${0}" ]] && SCRIPT_FILE="$(echo "${SCRIPT_FILE}" | cut -c 1-)"
+  SCRIPT_FILE="$(pwd)/$(echo -e "${SCRIPT}")"
+  [[ "${SCRIPT}" == "${0}" ]] && SCRIPT_FILE="$(echo -e "${SCRIPT_FILE}" | cut -c 1-)"
   SCRIPT_SHA="$(shasum -a 512 -b "${SCRIPT_FILE}" | awk '{ print $1 }')"
   BIN_SHA=""
   [[ -s "${SCRIPT_BIN}" ]] && BIN_SHA="$(shasum -a 512 -b "${SCRIPT_BIN}" | awk '{ print $1 }')"
@@ -141,8 +144,8 @@ validate_caller() {
 
 # macOS Version check
 check_compatibility() {
-  MACOS_MAJOR_VER="$(echo "${MACOS_VER}" | cut -d '.' -f2)"
-  MACOS_MINOR_VER="$(echo "${MACOS_VER}" | cut -d '.' -f3)"
+  MACOS_MAJOR_VER="$(echo -e "${MACOS_VER}" | cut -d '.' -f2)"
+  MACOS_MINOR_VER="$(echo -e "${MACOS_VER}" | cut -d '.' -f3)"
   [[ ("${MACOS_MAJOR_VER}" < 13) || ("${MACOS_MAJOR_VER}" == 13 && "${MACOS_MINOR_VER}" < 4) ]] && echo -e "\nOnly ${BOLD}macOS 10.13.4 or later${NORMAL} compatible.\n" && exit
   [[ "${MACOS_MAJOR_VER}" == 13 ]] && IS_HIGH_SIERRA=1
 }
@@ -183,18 +186,16 @@ manage_all_apps_prefs_in_folder() {
   while read APP
   do
     [[ "${APP}" =~ "${UTILITIES}" ]] && continue
-    APP_NAME="${APP##*/}"
-    APP_NAME="${APP_NAME%.*}"
-    BUNDLE_ID=$(osascript -e "id of app \"${APP_NAME}\"" 2>/dev/null)
+    BUNDLE_ID=$(osascript -e "id of app \"${APP}\"" 2>/dev/null)
     [[ -z "${BUNDLE_ID}" ]] && continue
     "${2}" "${BUNDLE_ID}"
-  done < <(find "${1}" -name "*.app")
+  done < <(find "${1}" -name "*.app" -prune 2>/dev/null)
 }
 
 # Manage preferences for all applications
 manage_all_apps_egpu() {
   echo -e "\n>> ${BOLD}${1} GPU Preferences for All Applications${NORMAL}\n\n${BOLD}${1}ting...${NORMAL}"
-  MESSAGE="$(echo "${1}" | awk '{ print tolower($0) }')"
+  MESSAGE="$(echo -e "${1}" | awk '{ print tolower($0) }')"
   if [[ $IS_HIGH_SIERRA == 0 ]]
   then
     [[ "${1}" == "Set" ]] && set_app_pref "-" 2>&1 && echo -e "Global preference ${MESSAGE}.\n" && return
@@ -202,36 +203,111 @@ manage_all_apps_egpu() {
   fi
   manage_all_apps_prefs_in_folder "/Applications" "${2}"
   manage_all_apps_prefs_in_folder "${HOME}/Applications" "${2}"
-  manage_all_apps_prefs_in_folder "/Library/Application Support" "${2}"
+  manage_all_apps_prefs_in_folder "${HOME}/Library" "${2}"
   echo -e "Preferences ${MESSAGE}.\n"
+}
+
+manage_pref_for_found_app() {
+  local APP_PATH="${1}"
+  local INTENT="${2}"
+  MESSAGE="$(echo -e "${INTENT}" | awk '{ print tolower($0) }')"
+  local FUNCTION="${3}"
+  local APP_PRINT_NAME="${APP_PATH##*/}"
+  local APP_PRINT_NAME="${APP_PRINT_NAME%.*}"
+  BUNDLE_ID=$(osascript -e "id of app \"${APP_PATH}\"" 2>/dev/null)
+  [[ -z "${BUNDLE_ID}" ]] && echo -e "\nTarget application does not exist. No action taken.\n" && return
+  echo -e "\n${BOLD}${INTENT}ting preference for ${APP_PRINT_NAME}...${NORMAL}"
+  "${FUNCTION}" "${BUNDLE_ID}"
+  echo -e "Preferences ${MESSAGE}.\n"
+}
+
+# Ask for specific application
+request_specific_app() {
+  local INTENT="${1}"
+  local FUNCTION="${2}"
+  echo
+  read -p "Which application? [1-${APP_COUNT} | Q]: " INPUT
+  [[ "${INPUT}" == "Q" ]] && echo -e "\nNo preferences changed.\n" && return
+  [[ ! "${INPUT}" =~ [0-9] ]] && echo -e "\nInvalid input. Try again." && request_specific_app "${APP_PATH}" "${INTENT}" "${FUNCTION}" && return
+  (( INPUT-- ))
+  (( $INPUT < 0 || $APP_COUNT <= $INPUT )) && echo -e "\nInvalid choice. Try again." && request_specific_app "${APP_PATH}" "${INTENT}" "${FUNCTION}" && return
+  manage_pref_for_found_app "${APPS_LIST[$INPUT]}" "${INTENT}" "${FUNCTION}"
 }
 
 # Manage preferences for specified application
 manage_specified_apps_egpu() {
   echo -e "\n>> ${BOLD}${1} GPU Preference for Specified Application(s)${NORMAL}\n"
-  MESSAGE="$(echo "${1}" | awk '{ print tolower($0) }')"
-  echo "Please use the ${BOLD}exact${NORMAL} application name as seen in Launchpad."
-  IFS= read -p "Enter application name: " INPUT
+  echo -e "Acronyms must be ${BOLD}all uppercase${NORMAL}.\nPartial names will return a list of possible applications.\nType ${BOLD}'QUIT'${NORMAL} to exit this submenu.\n"
+  MESSAGE="$(echo -e "${1}" | awk '{ print tolower($0) }')"
+  IFS= read -p "${BOLD}Application${NORMAL} Name or Acronym: " INPUT
   [[ -z "${INPUT}" ]] && echo -e "\nEmpty input provided. No action taken.\n" && return
-  BUNDLE_ID=$(osascript -e "id of app \"${INPUT}\"" 2>/dev/null)
-  [[ -z "${BUNDLE_ID}" ]] && echo -e "\nTarget application does not exist. No action taken.\n" && return
-  echo -e "\n${BOLD}${1}ting...${NORMAL}"
-  "${2}" "${BUNDLE_ID}"
-  echo -e "Preferences ${MESSAGE}.\n"
+  [[ "${INPUT}" == "quit" ]] && echo && return
+  GENERALIZED_APP_NAME=""
+  shopt -u nocasematch
+  if [[ ! "${INPUT}" =~ [a-z] && ! "${INPUT}" =~ " " ]]
+  then
+    GENERALIZED_APP_NAME="$(echo -e "${INPUT}" | sed -e 's/\(.\)/\1*/g')"
+  else
+    GENERALIZED_APP_NAME="*${INPUT// /*}*"
+  fi
+  shopt -s nocasematch
+  GENERALIZED_APP_NAME="${GENERALIZED_APP_NAME}.app"
+  APP_COUNT=0
+  while read APP
+  do
+    [[ "${APP}" =~ "${UTILITIES}" ]] && continue
+    [[ $APP_COUNT == 0 ]] && echo -e "\n${BOLD}Possible Matches${NORMAL}:"
+    APP_NAME="${APP##*/}"
+    APP_NAME="${APP_NAME%.*}"
+    APPS_LIST+=("${APP}")
+    (( APP_COUNT++ ))
+    printf "${BOLD}%-3d${NORMAL}:  %-s\n" "${APP_COUNT}" "${APP_NAME}"
+  done < <(find "/Applications" "${HOME}/Applications" "${HOME}/Library" \( -iname "*.app" -prune \) -iname "${GENERALIZED_APP_NAME}")
+  echo
+  case $APP_COUNT in
+    0)
+    echo -e "No matches found for this application.\n";;
+    1)
+    manage_pref_for_found_app "${APPS_LIST[0]}" "${1}" "${2}";;
+    *)
+    request_specific_app "${1}" "${2}";;
+  esac
+  APPS_LIST=()
 }
 
 # Check preferences for specified application
 check_app_preferences() {
   echo -e "\n>> ${BOLD}Check Application eGPU Preference${NORMAL}\n"
-  echo "Please use the ${BOLD}exact${NORMAL} application name as seen in Launchpad."
-  IFS= read -p "Enter application name: " INPUT
-  CURRENT_PREF=""
-  BUNDLE_ID=$(osascript -e "id of app \"${INPUT}\"" 2>/dev/null)
-  [[ -z "${BUNDLE_ID}" ]] && echo -e "\nTarget application does not exist. No action taken.\n" && return
-  [[ $IS_HIGH_SIERRA == 1 ]] && CURRENT_PREF="$(defaults read "${BUNDLE_ID}" "${GPU_SELECTION_POLICY_KEY}" 2>/dev/null)" || CURRENT_PREF="$(SafeEjectGPU evalPref ${BUNDLE_ID} ${GPU_SELECTION_POLICY_KEY} 2>/dev/null)"
-  [[ "${CURRENT_PREF}" == "${GPU_SELECTION_POLICY_KEY}=<not set>" || -z "${CURRENT_PREF}" ]] && CURRENT_PREF="No preference set."
-  [[ "${CURRENT_PREF}" == "${GPU_SELECTION_POLICY_KEY}=${GPU_SELECTION_POLICY_VALUE}" || "${CURRENT_PREF}" == "${GPU_SELECTION_POLICY_VALUE}" ]] && CURRENT_PREF="Prefers external GPUs."
-  echo -e "\n${BOLD}Status${NORMAL}: ${CURRENT_PREF}\n"
+  IFS= read -p "${BOLD}Application${NORMAL} Name or Acronym: " INPUT
+  [[ -z "${INPUT}" ]] && echo -e "\nPlease enter an application name.\n" && return
+  GENERALIZED_APP_NAME=""
+  shopt -u nocasematch
+  if [[ ! "${INPUT}" =~ [a-z] && ! "${INPUT}" =~ " " ]]
+  then
+    GENERALIZED_APP_NAME="$(echo -e "${INPUT}" | sed -e 's/\(.\)/\1*/g')"
+  else
+    GENERALIZED_APP_NAME="*${INPUT// /*}*"
+  fi
+  shopt -s nocasematch
+  GENERALIZED_APP_NAME="${GENERALIZED_APP_NAME}.app"
+  APP_COUNT=0
+  while read APP
+  do
+    [[ "${APP}" =~ "${UTILITIES}" ]] && continue
+    [[ $APP_COUNT == 0 ]] && echo
+    (( APP_COUNT++ ))
+    BUNDLE_ID=$(osascript -e "id of app \"${APP}\"" 2>/dev/null)
+    [[ -z "${BUNDLE_ID}" ]] && echo -e "\nTarget application does not exist. No action taken.\n" && return
+    APP_NAME="${APP##*/}"
+    APP_NAME="${APP_NAME%.*}"
+    CURRENT_PREF=""
+    [[ $IS_HIGH_SIERRA == 1 ]] && CURRENT_PREF="$(defaults read "${BUNDLE_ID}" "${GPU_SELECTION_POLICY_KEY}" 2>/dev/null)" || CURRENT_PREF="$(SafeEjectGPU evalPref ${BUNDLE_ID} ${GPU_SELECTION_POLICY_KEY} 2>/dev/null)"
+    [[ "${CURRENT_PREF}" == "${GPU_SELECTION_POLICY_KEY}=<not set>" || -z "${CURRENT_PREF}" ]] && CURRENT_PREF="does not prefer eGPUs."
+    [[ "${CURRENT_PREF}" == "${GPU_SELECTION_POLICY_KEY}=${GPU_SELECTION_POLICY_VALUE}" || "${CURRENT_PREF}" == "${GPU_SELECTION_POLICY_VALUE}" ]] && CURRENT_PREF="prefers external GPUs."
+    echo -e "${BOLD}${APP_NAME}${NORMAL} ${CURRENT_PREF}"
+  done < <(find "/Applications" "${HOME}/Applications" "${HOME}/Library" \( -iname "*.app" -prune \) -iname "${GENERALIZED_APP_NAME}")
+  echo
+  (( APP_COUNT == 0 )) && echo -e "No matching applications found for your search.\n"
 }
 
 # ----- DRIVER
@@ -247,7 +323,7 @@ ask_menu() {
 
 # Menu
 provide_menu_selection() {
-  echo "
+  echo -e "
    ${BOLD}1.${NORMAL}  Set eGPU Preference for All Applications
    ${BOLD}2.${NORMAL}  Set eGPU Preference for Specified Application(s)
    ${BOLD}3.${NORMAL}  Check Application eGPU Preference
@@ -256,7 +332,12 @@ provide_menu_selection() {
    ${BOLD}6.${NORMAL}  Quit
   "
   read -p "${BOLD}What next?${NORMAL} [1-6]: " INPUT
-  [[ ! -z "${INPUT}" ]] && process_args "${INPUT}" || echo -e "\nNo input provided.\n"
+  if [[ ! -z "${INPUT}" ]]
+  then
+    process_args "${INPUT}"
+  else
+    echo -e "\nNo input provided.\n"
+  fi
   ask_menu
 }
 
@@ -277,7 +358,7 @@ process_args() {
     echo && exit;;
     "")
     start_install
-    clear && echo ">> ${BOLD}Set-eGPU (${SCRIPT_VER})${NORMAL}"
+    clear && echo -e ">> ${BOLD}Set-eGPU (${SCRIPT_VER})${NORMAL}"
     provide_menu_selection;;
     *)
     echo -e "\nInvalid option.\n";;
